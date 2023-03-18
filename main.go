2103b22c62798ec7b6f79690ff9db2572de56e88
namikaze-dev <usmansadik96@gmail.com>
2023-03-18 21:46:21 +0100

    Implement parseCSV func

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"encoding/csv"
+	"io"
+	"os"
+	"strconv"
+)
+
 type Grade string
 
 const (
@@ -21,7 +28,40 @@ type studentStat struct {
 }
 
 func parseCSV(filePath string) []student {
-	return nil
+	rd, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	csvRd := csv.NewReader(rd)
+	records, err := csvRd.ReadAll()
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+
+	var students []student
+	for _, row := range records[1:] {
+		var student student
+		student.firstName = row[0]
+		student.lastName = row[1]
+		student.university = row[2]
+		student.test1Score = parseInt(row[3])
+		student.test2Score = parseInt(row[4])
+		student.test3Score = parseInt(row[5])
+		student.test4Score = parseInt(row[6])
+		students = append(students, student)
+	}
+
+	return students
+}
+
+func parseInt(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return n
 }
 
 func calculateGrade(students []student) []studentStat {
